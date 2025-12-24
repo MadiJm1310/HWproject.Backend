ffdb9c36c803d8ab1b37f05b379f230308345441
MadiJm1310 <mmamedova660@gmail.com>
2025-12-25 01:02:56 +0400

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_HOST', 'mysql-madina0mammadova.alwaysdata.net');
+define('DB_NAME', 'madina0mammadova_hw_project');
+define('DB_USER', '410053_user');
+define('DB_PASS', 'xxxx');
+define('DB_CHARSET', 'utf8mb4');
+?>
